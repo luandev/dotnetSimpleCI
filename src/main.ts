2b35dfc4b28c330bf0455f31b671d00377d3b9ce
Luan Città <luan.dev@gmail.com>
2018-08-20 14:31:27 +0100

    Init commit

---
+++ src/main.ts
@@ -0,0 +1,102 @@
+// import * as git from 'git'
+import { exec } from 'child_process';
+import { GitWatcher, RepoResult, RepoWatchConfig } from 'git-repo-watch';
+// import { exec } from 'child_process';
+
+exec(`git clone ${process.argv[3]} ${process.argv[2]}`);
+
+const gw = new GitWatcher();
+
+// tslint:disable-next-line:no-console
+console.log(process.argv[0]);
+
+// Use Sync Fork to check for changes in the upstream an update.
+gw.watch({
+	path: process.argv[2],
+	remote: process.argv[3],
+	branch: 'master',
+	poll: 5,
+	strict: true,
+	sync: {
+		remote: process.argv[3],
+		branch: 'master',
+	}
+});
+
+gw.check$.subscribe((info: RepoWatchConfig) => {
+
+	// tslint:disable-next-line:no-console
+	console.log(info);
+
+	// will fire every check.
+});
+
+gw.result$.subscribe((result: RepoResult) => {
+
+	try {
+		// tslint:disable-next-line:no-console
+		console.log(result);
+
+		if (result.error) {
+			gw.unwatch(result.config);
+		} else {
+			if (result.changed === true) {
+				// tslint:disable-next-line:no-console
+				console.log("result");
+			}
+		}
+	}
+	catch{
+		// tslint:disable-next-line:no-console
+		console.log(result);
+	}
+
+
+});
+
+
+
+// import moment from 'moment';
+
+
+// /**
+//  * Some predefined delays (in milliseconds).
+//  */
+// export enum Delays {
+
+
+
+//   Short = 500,
+//   Medium = 2000,
+//   Long = 5000,
+// }
+
+// /**
+//  * Returns a Promise<string> that resolves after given time.
+//  *
+//  * @param {string} name - A name.
+//  * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
+//  * @returns {Promise<string>}
+//  */
+// function delayedHello(
+//   name: string,
+//   delay: number = Delays.Medium,
+// ): Promise<string> {
+//   return new Promise((resolve: (value?: string) => void) =>
+//     setTimeout(() => resolve(`Hello, ${name}`), delay),
+//   );
+// }
+
+// // Below are examples of using TSLint errors suppression
+// // Here it is suppressing missing type definitions for greeter function
+
+// // tslint:disable-next-line typedef
+// export async function greeter(name) {
+
+//   // tslint:disable-next-line:no-console
+//   console.log(moment(new Date()).toString())
+
+
+//   // tslint:disable-next-line no-unsafe-any no-return-await
+//   return await delayedHello(name, Delays.Long);
+// }
